8ba06851d75a88a6ede88c2979a56f0b426b9ca4
ChenyangDu <52902587+ChenyangDu@users.noreply.github.com>
2020-08-11 23:26:02 +0800

    解决is开头变量的bug

--- src/main/java/com/springboot/demo/entity/Document.java
+++ src/main/java/com/springboot/demo/entity/Document.java
@@ -3,6 +3,7 @@ package com.springboot.demo.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -22,4 +23,20 @@ public class Document {
     private boolean is_deleted;
     private boolean is_editting;
     private String name;
+
+    public boolean isIs_deleted() {
+        return is_deleted;
+    }
+
+    public boolean isIs_editting() {
+        return is_editting;
+    }
+
+    public void setIs_deleted(boolean is_deleted) {
+        this.is_deleted = is_deleted;
+    }
+
+    public void setIs_editting(boolean is_editting) {
+        this.is_editting = is_editting;
+    }
 }
