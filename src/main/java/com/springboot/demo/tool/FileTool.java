ed6c87d15ea803199d5ec1b3cf3896611c372163
ChenyangDu <52902587+ChenyangDu@users.noreply.github.com>
2020-08-11 14:54:47 +0800

    文件读写工具
    封装一下，用起来方便

---
+++ src/main/java/com/springboot/demo/tool/FileTool.java
@@ -0,0 +1,60 @@
+package com.springboot.demo.tool;
+
+import java.io.*;
+
+public class FileTool {
+    /**
+     * 将文本文件中的内容读入到buffer中
+     * @param buffer buffer
+     * @param filePath 文件路径
+     * @throws IOException 异常
+     * @author cn.outofmemory
+     * @date 2013-1-7
+     */
+    public static void readToBuffer(StringBuffer buffer, String filePath) throws IOException {
+        InputStream is = new FileInputStream(filePath);
+        String line; // 用来保存每行读取的内容
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        line = reader.readLine(); // 读取第一行
+        while (line != null) { // 如果 line 为空说明读完了
+            buffer.append(line); // 将读到的内容添加到 buffer 中
+            buffer.append("\n"); // 添加换行符
+            line = reader.readLine(); // 读取下一行
+        }
+        reader.close();
+        is.close();
+    }
+
+    /**
+     * 读取文本文件内容
+     * @param filePath 文件所在路径
+     * @return 文本内容
+     * @throws IOException 异常
+     * @author cn.outofmemory
+     * @date 2013-1-7
+     */
+    public static String readFile(String filePath) throws IOException {
+        StringBuffer sb = new StringBuffer();
+        FileTool.readToBuffer(sb, filePath);
+        return sb.toString();
+    }
+
+    /**
+     * 保存文件内容
+     *
+     */
+    public static void writeFile(String filePath,String content) throws IOException{
+
+        File file = new File(filePath);
+
+        // if file doesnt exists, then create it
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileWriter fw = new FileWriter(file.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(content);
+        bw.close();
+    }
+}
