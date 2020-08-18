d57a6b0905846ef92ff58b8541e8b4fb07612925
MindLostGuy <1575747105@qq.com>
2020-08-18 15:47:17 +0800

    debug

--- src/main/java/com/springboot/demo/controller/UserController.java
+++ src/main/java/com/springboot/demo/controller/UserController.java
@@ -234,6 +234,14 @@ public class UserController {
         private Integer groups;
         private Integer views;
         public UserFuck(User user){
+            this.id=user.getId();
+            this.email=user.getEmail();
+            this.name=user.getPassword();
+            this.password=user.getPassword();
+            this.phone=user.getPhone();
+            this.qq=user.getQq();
+            this.sign=user.getSign();
+            this.wechat=user.getWechat();
             Document tmpDocu=new Document();
             tmpDocu.setCreator_id(id);
             tmpDocu.setIs_deleted(false);
